d1f26c251b6a6f2e9ca2f4aca087515b2ad779eb
ozaki <fs65ugumori@yahoo.co.jp>
2019-02-21 08:57:38 +0900

    add start and stop api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 const api = process.env.REACT_APP_API;
 const getInstancesApi = `${api}instances`;
+const startInstancesApi = `${api}instances/start`;
+const stopInstancesApi = `${api}instances/stop`;
 
 const getInstances = async () => {
   try {
@@ -15,6 +17,42 @@ const getInstances = async () => {
   }
 };
 
+const startInstances = async body => {
+  try {
+    const res = await fetch(startInstancesApi, {
+      method: 'POST',
+      body: JSON.stringify(body),
+    });
+    if (res.ok) {
+      return res.json();
+    } else {
+      console.log(res);
+    }
+  } catch (e) {
+    alert(e.toString());
+    console.log(e);
+  }
+};
+
+const stopInstances = async body => {
+  try {
+    const res = await fetch(stopInstancesApi, {
+      method: 'POST',
+      body: JSON.stringify(body),
+    });
+    if (res.ok) {
+      return res.json();
+    } else {
+      console.log(res);
+    }
+  } catch (e) {
+    alert(e.toString());
+    console.log(e);
+  }
+};
+
 export default {
   getInstances,
+  startInstances,
+  stopInstances,
 };
