d1e66bce9b198cc2495e1ca5b834d4471a233805
ozaki <fs65ugumori@yahoo.co.jp>
2019-02-20 23:48:01 +0900

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Navbar } from '@blueprintjs/core';
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Navbar className="bp3-dark">
+      <Navbar.Group>
+        <Navbar.Heading>EC2 Manager</Navbar.Heading>
+        <Navbar.Divider />
+      </Navbar.Group>
+    </Navbar>
+  );
 }
 
 export default App;
